f85ce246775403675d850567dd0ecd37fffc9516
sarcastic-verma <shivamthegreat.sv@gmail.com>
2020-07-18 00:27:15 +0530

    added leaderboard route

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -8,6 +8,7 @@ const checkAuth = require('../middleware/check-auth');
 const router = express.Router();
 
 router.get('/', usersController.getUsers);
+router.get('/leaderBoard', usersController.getLeaderBoard);
 
 router.get('/:uid', usersController.getUserById);
 
@@ -31,7 +32,6 @@ router.post('/login', usersController.login);
 router.patch('/forgotPassword/:email', [
     check('password').isLength({min: 6})
 ], usersController.forgotPassword);
-
 router.use(checkAuth);
 router.patch('/edit/:uid', fileUpload.single('image'),
     [
