844b5e2e4fcf43379b97d1dcffdf39a031f10eb8
dramikei <raghav2011rv@gmail.com>
2020-10-02 02:26:53 +0530

    Improved http-error middleware with better serverSide error logging

--- models/http-error.js
+++ models/http-error.js
@@ -1,8 +1,27 @@
 class HttpError extends Error {
-  constructor(message, errorCode) {
+  constructor(message, errorCode, err, ip) {
     super(message);
     this.code = errorCode;
-  }
+
+    // If err is passed, it will be logged on the console
+    if (err) {
+        console.log("\n")
+        const currentdate = new Date(); 
+        const datetime = currentdate.getDate() + "/"
+                        + (currentdate.getMonth()+1)  + "/" 
+                        + currentdate.getFullYear() + " @ "  
+                        + currentdate.getHours() + ":"  
+                        + currentdate.getMinutes() + ":" 
+                        + currentdate.getSeconds();
+        if (ip) {
+          console.log(`The following error occured on ${datetime}: while processing request from ip: ${ip}`);
+        } else {
+          console.log(`The following error occured on ${datetime}:`);
+        }
+        console.log(err);
+        console.log("\n")
+    }
+}
 }
 
 module.exports = HttpError;
